69fe7d307e32c67f0653833ad50b2346c1283c9a
user <user@users-MacBook-Pro.local>
2021-03-02 16:24:48 -0500

    add find employee bt its id, and find employee working for compnayID

--- resolver.js
+++ resolver.js
@@ -1,8 +1,18 @@
 const db = require('./db')  
 const Query = {  
-   hello:() => {  
-      return "Welcome to Graphql Skeleton"  
-   },  
-   employees:() => db.employees.list()  
+   // find all emplyees 
+   employees:() => db.employees.list(),
+   // find all companies
+   companies:() => db.companies.list() ,
+   // find an employee by its employees
+   employeesById: (obj, args, context, info) => {
+      console.log(args.id)
+      return db.employees.list().filter((employee => employee.id === args.id))
+   },
+   // gind all employees of company whose companyId is args.companyId
+   employeesAtCoompany: (obj, args, context, info) => {
+      console.log(args.companyId)
+      return db.employees.list().filter((employee=> employee.companyId === args.companyId))
+   }
 }  
 module.exports = {Query}  
